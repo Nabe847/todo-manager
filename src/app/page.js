f50fa65f607f4ab3795a1ae796afd211ab4eb4a1
Nabe847 <manabetakumi02@gmail.com>
2025-06-14 21:09:13 +0900

    feat: step3

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
-// 1. コンポーネントの定義
+// 1.CSSモジュールのインポート
+import style from './page.module.css';
+
 export default function Home() {
   return (
-    // 2. 画面の見た目の定義（HTMLのような記法で記述）
-    <div>
-      <h1>はじめてのTodoアプリ</h1>
-      <p>やることリストを簡単に管理できます</p>
-      <button>始める</button>
+    // 2.スタイルの適用
+    <div className={style.container}>
+      <h1 className={style.heading}>はじめてのTodoアプリ</h1>
+      <p className={style.box}>やることリストを簡単に管理できます</p>
+      <button className={style.button}>始める</button>
     </div>
   );
 }
